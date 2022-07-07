591a55b61ed5c0373cbeacd759df41ad1d2c2579
LzoWasTaken <lzo22@protonmail.com>
2022-07-06 21:47:39 -0400

    Implemented while loop

--- RandomNumberGenerator/main.c
+++ RandomNumberGenerator/main.c
@@ -9,23 +9,31 @@ int main() {
 	
 	int randomNumber = rand() % 21;
 	int guess;
+	int guesses = 5;
 
 	puts("Try to guess a number between 1 and 20!");
+	
+	while (guesses > 0) {
+		puts("\n\nOk, please enter your guess: ");
+		scanf("%i", &guess);
 
-	puts("\n\nOk, please enter your guess: ");
-	scanf("%i", &guess);
+		if (guess == randomNumber) {
+			puts("You guessed correctly!");
+			break; 
+		} else if (guess > 20 || guess < 0) {
+			puts("Your guess is out of range. You are to guess a number between 1 and 20.");
+			continue;
+		} else if (guess > randomNumber)
+			printf("%i is too high... The number is less than that", guess);
 
-	if (guess == randomNumber)
-		puts("You guessed correctly!");
+		else if (guess < randomNumber)
+			printf("%i is too low... The number is greater than that", guess);
+		
+		guesses--;	
+	} if (guesses == 0) {
+		printf("\nYou're out of guesses... The number that I was thinking of is %i\n", randomNumber);
 
-	else if (guess > 20 || guess < 0)
-		puts("Your guess is out of range. You are to guess a number between 0 and 20.");
-
-	else if (guess > randomNumber)
-		printf("%i is too high... The number is less than that", guess);
-
-	else if (guess < randomNumber)
-		printf("%i is too low... The number is greater than that", guess);
+	    }
 	
 	return 0;
 }
